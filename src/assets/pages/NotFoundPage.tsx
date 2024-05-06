20fd34c573982c7bd9a125d0355faf4492d902fc
Jasmin Ramirez <ms.jasmin1999@gmail.com>
2024-05-05 22:07:03 -0400

    Conditional class

--- src/assets/pages/NotFoundPage.tsx
+++ src/assets/pages/NotFoundPage.tsx
@@ -1,4 +1,5 @@
 import { FaExclamationTriangle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
@@ -6,6 +7,12 @@ const NotFoundPage = () => {
       <FaExclamationTriangle className="text-yellow-400 text-6xl mb-4" />
       <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
       <p className="text-xl mb-5">This page does not exist</p>
+      <Link
+        to="/"
+        className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
+      >
+        Go Back
+      </Link>
     </section>
   );
 };
